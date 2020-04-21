ddfece7a8e62c1862145a8034b3885e84737bbae
Gabby Campos <gabbycampos@Gabbys-MacBook-Pro.local>
2020-04-21 17:37:31 -0500

    Updates

---
+++ precache-manifest.30071e81621d096ee572640adbbda1ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abf1d5c47c8cbadf0333907f17d53ea3",
+    "url": "/calculator-hooks/index.html"
+  },
+  {
+    "revision": "3e86bc24ba997a69d974",
+    "url": "/calculator-hooks/static/css/main.5204c198.chunk.css"
+  },
+  {
+    "revision": "e530ff2237f107b26e67",
+    "url": "/calculator-hooks/static/js/2.d2bc3ba2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator-hooks/static/js/2.d2bc3ba2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e86bc24ba997a69d974",
+    "url": "/calculator-hooks/static/js/main.0f253867.chunk.js"
+  },
+  {
+    "revision": "842834ecf8f8d2fb59f0",
+    "url": "/calculator-hooks/static/js/runtime-main.719c2191.js"
+  }
+]);
